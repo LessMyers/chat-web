cc03e6bfa5846e0923669c93e7991033a8a3ad16
lessiebmyers <lessiebmyers@gmail.com>
2025-08-28 01:27:12 +0800

    1

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,26 +1,22 @@
 <template>
     <div style="background-color:#f7f8fa; padding-top: 6px; padding-bottom: 6px;">
         <template v-for="msg in messages">
-            <!-- 1:  -->
             <van-row v-if="msg.type == Type.Text">
-                <van-col span="2" style="text-align: center; padding-top: 5px;">
+                <van-col span="2" class="side">
                     <van-icon v-if="u_id == msg.receiver" name="like" size="25" color="#1989fa" />
                 </van-col>
                 <van-col span="20" v-if="u_id == msg.sender" class="is-self">
-                    <div style="white-space: pre-line; word-break: break-all; background-color: #90ee90; padding: 8px; border-radius: 4px; display: inline-block;"
-                        v-html="text_time(msg.text, msg.utc)">
+                    <div class="text" style="background-color: #90ee90;" v-html="text_time(msg.text, msg.utc)">
                     </div>
                 </van-col>
                 <van-col span="20" v-else>
-                    <div
-                        style="color: #323233; font-size: 10px; line-height: 10px; padding: 8px; display: inline-block;">
+                    <div class="name">
                         {{ get_name(msg.sender) }}</div>
                     <br />
-                    <div style="white-space: pre-line; word-break: break-all; background-color: white; padding: 8px; border-radius: 4px; display: inline-block;"
-                        v-html="text_time(msg.text, msg.utc)">
+                    <div class="text" v-html="text_time(msg.text, msg.utc)">
                     </div>
                 </van-col>
-                <van-col span="2" style="text-align: center; padding-top: 5px;">
+                <van-col span="2" class="side">
                     <van-icon v-if="u_id == msg.sender" name="star" size="25" color="#ee0a24" />
                 </van-col>
             </van-row>
@@ -30,7 +26,7 @@
             </van-row>
             <van-row v-else-if="msg.type == Type.Image">
             </van-row>
-            <br/>
+            <br />
         </template>
     </div>
 </template>
@@ -52,10 +48,10 @@ const messages = ref([
 ]);
 
 const Type = {
-  Text: 1,
-  Audio: 2,
-  Video: 3,
-  Image: 4,
+    Text: 1,
+    Audio: 2,
+    Video: 3,
+    Image: 4,
 };
 
 function text_time(text, utc) {
@@ -83,5 +79,27 @@ function get_name(id) {
     display: inline-block;
     margin-left: 6px;
     transform: translateY(6px);
+}
+
+.name {
+    color: #323233;
+    font-size: 10px;
+    line-height: 10px;
+    padding: 8px;
+    display: inline-block;
+}
+
+.text {
+    white-space: pre-line;
+    word-break: break-all;
+    background-color: white;
+    padding: 8px;
+    border-radius: 4px;
+    display: inline-block;
+}
+
+.side {
+    text-align: center;
+    padding-top: 5px;
 }
 </style>

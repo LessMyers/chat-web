b21c44f984ee09cfc0fee1a4208d708bd3d3daa2
lessiebmyers <lessiebmyers@gmail.com>
2025-08-27 23:14:15 +0800

    1

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,17 +1,22 @@
 <template>
-    <div style="background-color:aquamarine">
-        <ul>
-            <li v-for="(item, index) in items" :key="index">
-                <br />
-                {{ item }}
-                <br />
-            </li>
-        </ul>
+    <div style="background-color:#f7f8fa">
+        <van-row v-for="msg in messages">
+            <van-col span="2">{{ msg.receiver }}</van-col>
+            <van-col span="20">{{ msg.text }}</van-col>
+            <van-col span="2">{{ msg.sender }}</van-col>
+        </van-row>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+const u_id = ref(1);
+const messages = ref([
+    { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306120, group: 0 },
+    { sender: 2, receiver: 1, type: 1, url: "", text: "你好", utc: 1756306220, group: 0 },
+    { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306320, group: 0 },
+    { sender: 2, receiver: 1, type: 1, url: "", text: "你好", utc: 1756306420, group: 0 },
+    { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306920, group: 0 },
+]);
 
-const items = ref(['苹果1', '香蕉2', '橙子3', '苹果', '香蕉', '橙子', '苹果', '香蕉', '橙子', '苹果', '香蕉', '橙子', '苹果', '香蕉', '橙子', '苹果', '香蕉', '橙子0']);
 </script>

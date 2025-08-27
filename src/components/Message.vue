b40ae9786190759a3c2e03b094f094066cdb786b
lessiebmyers <lessiebmyers@gmail.com>
2025-08-28 01:09:51 +0800

    1

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,7 +1,8 @@
 <template>
     <div style="background-color:#f7f8fa; padding-top: 6px; padding-bottom: 6px;">
         <template v-for="msg in messages">
-            <van-row>
+            <!-- 1:  -->
+            <van-row v-if="msg.type == Type.Text">
                 <van-col span="2" style="text-align: center; padding-top: 5px;">
                     <van-icon v-if="u_id == msg.receiver" name="like" size="25" color="#1989fa" />
                 </van-col>
@@ -13,7 +14,7 @@
                 <van-col span="20" v-else>
                     <div
                         style="color: #323233; font-size: 10px; line-height: 10px; padding: 8px; display: inline-block;">
-                        张三</div>
+                        {{ get_name(msg.sender) }}</div>
                     <br />
                     <div style="white-space: pre-line; word-break: break-all; background-color: white; padding: 8px; border-radius: 4px; display: inline-block;"
                         v-html="text_time(msg.text, msg.utc)">
@@ -23,6 +24,13 @@
                     <van-icon v-if="u_id == msg.sender" name="star" size="25" color="#ee0a24" />
                 </van-col>
             </van-row>
+            <van-row v-else-if="msg.type == Type.Audio">
+            </van-row>
+            <van-row v-else-if="msg.type == Type.Video">
+            </van-row>
+            <van-row v-else-if="msg.type == Type.Image">
+            </van-row>
+            <br/>
         </template>
     </div>
 </template>
@@ -35,13 +43,32 @@ const messages = ref([
         sender: 1, receiver: 2, type: 1, url: "", text: "你好这两种语法等效吗qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq?这两种语法等效吗?这两种语法等效吗?这两种语法等效吗?这两种语法等效吗?这两种语法等效吗?", utc: 1756306120, group: 0
     },
     { sender: 2, receiver: 1, type: 1, url: "", text: "你好", utc: 1756306220, group: 0 },
+    { sender: 2, receiver: 1, type: 1, url: "", text: "你好", utc: 1756306220, group: 0 },
     { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306320, group: 0 },
     { sender: 2, receiver: 1, type: 1, url: "", text: "你好", utc: 1756306420, group: 0 },
     { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306920, group: 0 },
+    { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306920, group: 0 },
+    { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306920, group: 0 }, { sender: 1, receiver: 2, type: 1, url: "", text: "你好", utc: 1756306920, group: 0 },
 ]);
+
+const Type = {
+  Text: 1,
+  Audio: 2,
+  Video: 3,
+  Image: 4,
+};
 
 function text_time(text, utc) {
     return `${text}<span class="time"> 21:18<span>`;
+}
+
+function get_name(id) {
+    if (id == 1) {
+        return "张三";
+    } else {
+        return "李四";
+    }
+
 }
 </script>
 
@@ -49,6 +76,7 @@ function text_time(text, utc) {
 .is-self {
     text-align: right;
 }
+
 .time {
     font-size: 12px;
     color: #969799;
